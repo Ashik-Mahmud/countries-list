5b09d8efba6eee041a74dce54aff6bce96bffc64
ashikmahmud <ashikmamud187@gmail.com>
2022-02-25 20:37:58 +0600

    yeah changes something for performance

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,7 +112,7 @@ const openModal = () => {
 const loadModalData = (capital) => {
     fetch(`https://restcountries.com/v3.1/alpha/${capital}`)
         .then(response => response.json())
-        .then(data => displayModalInfo(data[0]))  
+        .then(data => displayModalInfo(data[0]))
 }
 
 /* step 6 display modal info  */
@@ -227,12 +227,5 @@ document.addEventListener('click', (event) => {
 })
 
 
-
-
-
-
-
-
 /* last step calling all required function  */
-
 loadCountries();
